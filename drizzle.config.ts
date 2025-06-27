5cdb10d3b860ff5693bc19de44ebf232ed3057b4
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-06-27 12:16:14 -0400

    style: replace tabs with spaces

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,16 +2,16 @@ import "dotenv/config"
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-	out: "./drizzle",
-	schema: "./src/db/schema.ts",
-	dialect: "sqlite",
-	driver: "d1-http",
-	dbCredentials: {
-		// biome-ignore lint/style/noNonNullAssertion: errors if missing.
-		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-		// biome-ignore lint/style/noNonNullAssertion: errors if missing.
-		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-		// biome-ignore lint/style/noNonNullAssertion: errors if missing.
-		token: process.env.CLOUDFLARE_D1_TOKEN!,
-	},
+  out: "./drizzle",
+  schema: "./src/db/schema.ts",
+  dialect: "sqlite",
+  driver: "d1-http",
+  dbCredentials: {
+    // biome-ignore lint/style/noNonNullAssertion: errors if missing.
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    // biome-ignore lint/style/noNonNullAssertion: errors if missing.
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    // biome-ignore lint/style/noNonNullAssertion: errors if missing.
+    token: process.env.CLOUDFLARE_D1_TOKEN!,
+  },
 })
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,10 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core"
 
 export const buckets = sqliteTable("buckets", {
-	id: int().primaryKey({ autoIncrement: true }),
-	endpoint: text().notNull(),
-	name: text().notNull().unique(),
-	accessKeyId: text().notNull(),
-	secretAccessKey: text().notNull(),
-	region: text().notNull(),
+  id: int().primaryKey({ autoIncrement: true }),
+  endpoint: text().notNull(),
+  name: text().notNull().unique(),
+  accessKeyId: text().notNull(),
+  secretAccessKey: text().notNull(),
+  region: text().notNull(),
 })
