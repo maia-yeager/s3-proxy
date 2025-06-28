122decca81df2fd0c2d4509b7e5ba893137ff08a
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-06-28 13:31:42 -0400

    refactor: prepare for adding admin interface

---
+++ src/schema.ts
@@ -0,0 +1,11 @@
+import * as z from "zod/dist/types/v4"
+
+export const kvSchema = z.preprocess(
+  (value) => (typeof value === "string" ? JSON.parse(value) : null),
+  z.object({
+    endpoint: z.url().min(1),
+    accessKeyId: z.string().min(1),
+    secretAccessKey: z.string().min(1),
+    region: z.string().min(1),
+  }),
+)
