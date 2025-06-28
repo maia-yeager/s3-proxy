42e5b8c7a47c97758f1536290521d3fff1ea632f
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-06-28 15:53:57 -0400

    feat: add root admin redirect

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,23 @@ import { proxy } from "./proxy"
 export default {
   async fetch(request: Request, env: Env, _ctx: ExecutionContext) {
     const url = new URL(request.url)
+
+    // Redirect empty path and search to admin page.
+    if (
+      url.hostname === env.WORKER_HOSTNAME &&
+      url.pathname === "/" &&
+      url.search === ""
+    ) {
+      return Response.redirect(`https://${env.WORKER_HOSTNAME}/admin`, 307)
+    }
+    // Admin management page.
     if (
       url.hostname === env.WORKER_HOSTNAME &&
       url.pathname.startsWith("admin/")
     ) {
       return new Response("Not implemented", { status: 501 })
     }
+    // S3 proxy.
     return proxy(url, request, env)
   },
 }
