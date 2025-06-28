95e267f76f1a48e8417e20c71e16cc5385b4a9fd
Micah Yeager <42281448+micah-yeager@users.noreply.github.com>
2025-06-28 17:09:47 -0400

    refactor: simplify logic

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,21 @@
 import { proxy } from "./proxy"
 
 export default {
-  async fetch(request: Request, env: Env, _ctx: ExecutionContext) {
+  async fetch(request, env: Env) {
     const url = new URL(request.url)
 
-    // Redirect empty path and search to admin page.
-    if (
-      url.hostname === env.WORKER_HOSTNAME &&
-      url.pathname === "/" &&
-      url.search === ""
-    ) {
-      return Response.redirect(`https://${env.WORKER_HOSTNAME}/admin/`, 307)
+    if (url.hostname === env.WORKER_HOSTNAME) {
+      // Redirect empty path and search to admin page.
+      if (url.pathname === "/" && url.search === "") {
+        return Response.redirect(`https://${env.WORKER_HOSTNAME}/admin/`, 307)
+      }
+      // Admin management page. Use a 404 to force Worker to fall back to SPA mode.
+      if (url.pathname.startsWith("/admin")) {
+        return new Response("Not found", { status: 404 })
+      }
     }
-    // Admin management page.
-    if (
-      url.hostname === env.WORKER_HOSTNAME &&
-      url.pathname.startsWith("/admin")
-    ) {
-      return new Response("Not implemented", { status: 501 })
-    }
+
     // S3 proxy.
     return proxy(url, request, env)
   },
-}
+} satisfies ExportedHandler<Env>
